5cccabff0e4cd5aa6f4f793405b29b80d5c48497
Brock Barber <barberbt22@uww.edu>
2016-02-23 15:52:13 -0600

    Update CharacterFactory.cpp

--- zombiewar_cpp/CharacterFactory.cpp
+++ zombiewar_cpp/CharacterFactory.cpp
@@ -1,64 +1,33 @@
 /* 
- * File:   ZombieWar.cpp
- * Author: thaoc, brock
+ * File:   CharacterFactory.cpp
+ * Author: thaoc
  * 
- * Created on May 21, 2015, 1:54 PM
+ * Created on May 21, 2015, 1:31 PM
  */
 
-#include <iostream>
-#include "ZombieWar.h"
 #include "CharacterFactory.h"
 
-
-ZombieWar::ZombieWar() {
+CharacterFactory::CharacterFactory() {
 }
 
-ZombieWar::ZombieWar(const ZombieWar& orig) {
+CharacterFactory::CharacterFactory(const CharacterFactory& orig) {
 }
 
-ZombieWar::~ZombieWar() {
+CharacterFactory::~CharacterFactory() {
+    i = new CharacterFactory();
 }
 
-/**
- * Generates a random number of zombies.
- * @return a pointer to an array of zombies
- */
-IZombie * ZombieWar::randomZombies(){
+ICharacter* CharacterFactory::makeCharacter(std::string type){
+
+    //make a character based on type
+    //e.g. if type is "common infected", make CommonInfect
     
-    CharacterFactory *f = CharacterFactory::instance();
-    return 0;
+    return NULL;
 }
 
-/**
- * Generates a random set of survivors
- * @return a pointer to an array of survivors
- */
-ISurvivor * ZombieWar::randomSurvivors(){
-   
-    CharacterFactory *f = CharacterFactory::instance();
-    return 0;
-}
+CharacterFactory * CharacterFactory::i = 0;
 
-/**
- * Check if all characters in the array are all dead.
- * @param array the array of characters
- * @return true if none is alive.
- */
-bool ZombieWar::allDead(ICharacter * array){
-    return false;
-}
-
-void ZombieWar::start(){
-    std::cout << "Welcome to ZombieWar" << std::endl;
-    IZombie * zombies = this->randomZombies();
-    ISurvivor * survivors = this->randomSurvivors();
-    
-    //for each survivor, make it attack each zombie in the zombies array
-    //for each zombie, make it attack each survivor.
-    //repeat this process until one side is all dead.
-    //make sure dead character does not attack
-    //print the number of survivors that are still alive.
-    
-
-    
+CharacterFactory* CharacterFactory::instance(){
+    if (!i) i = new CharacterFactory();
+    return i;
 }
