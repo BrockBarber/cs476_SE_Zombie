d7d7e744c81885d9c03414ee4f65dd128883acc6
lilburr <thatcheraj03@uww.edu>
2016-02-27 16:50:47 -0600

    Create Tank.cpp
    Created a class for Tank zombie. Not sure I have uploaded this correctly. Feel free to make any changes needed.

---
+++ Tank.cpp
@@ -0,0 +1,20 @@
+//Tank Class
+//Author: Amber T.
+
+#include <iostream>
+#include "IZombie.h"
+#include "ICharacter.h"
+#include "ISurvivor.h"
+
+using namespace std;
+
+Class Tank: public Zombie {
+  public Tank();
+  int health = 150;
+  public Tank(int health) {
+    this.health = health;
+  }
+  public void attack(ISurvivor* survivor) {
+    survivor.decreaseHealth(20)
+  }
+}
