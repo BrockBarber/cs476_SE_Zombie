7dc4304f9cbee7d9717ebe3f30c805408f2298f4
Brock Barber <barberbt22@uww.edu>
2016-02-29 17:03:34 -0600

    Update CharacterFactory.cpp

--- zombiewar_cpp/CharacterFactory.cpp
+++ zombiewar_cpp/CharacterFactory.cpp
@@ -1,29 +1,22 @@
-/* 
- * File:   CharacterFactory.cpp
- * Author: Thao, Brock
- * 
- * Created on May 21, 2015, 1:31 PM
- */
-
+/*
+* File:   CharacterFactory.cpp
+* Author: Thao, Brock
+*
+* Created on May 21, 2015, 1:31 PM
+*/
 #include "CharacterFactory.h"
-
 CharacterFactory::CharacterFactory() {
 }
 
-std::unique_ptr<ICharacter> CharacterFactory::makeCharacter(std::string type){
+ICharacter* CharacterFactory::makeCharacter(std::string type){
 
-    //make a character based on type
-    //e.g. if type is "common infected", make CommonInfect
-	switch(type){
-		case "child":
-			return std::unique_ptr<ICharacter> = make_unique<Child>();
-			break;
-		case "Zombie":
-			return std::unique_ptr<ICharacter> = make_unique<Zombie>();
-			break;
-		default:
-			//do nothing yet
-			break;
+	//make a character based on type
+	//e.g. if type is "common infected", make CommonInfect
+	if (type == "child"){
+		return &Child();
 	}
-    return NULL;
+	if(type == "zombie")
+		return &Zombie();
+	}
+	return NULL;
 }
