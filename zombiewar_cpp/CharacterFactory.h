220543c564f494862684ac5652f47e682ee4d5ba
Brock Barber <barberbt22@uww.edu>
2016-02-29 17:04:24 -0600

    Update CharacterFactory.h

--- zombiewar_cpp/CharacterFactory.h
+++ zombiewar_cpp/CharacterFactory.h
@@ -1,23 +1,23 @@
-/* 
- * File:   CharacterFactory.h
- * Author: Thao, Brock
- *
- * Created on May 21, 2015, 1:31 PM
- * 
- * A character factory for creating different characters.
- * The factory encapsulation the character implementations.
- * 
- */
+/*
+* File:   CharacterFactory.h
+* Author: Thao, Brock
+*
+* Created on May 21, 2015, 1:31 PM
+*
+* A character factory for creating different characters.
+* The factory encapsulation the character implementations.
+*
+*/
 
 #ifndef CHARACTERFACTORY_H
 #define	CHARACTERFACTORY_H
 
 #include<string>
 #include "ICharacter.h"
+#include "Child.h"
 
 class CharacterFactory {
-    unique_ptr<ICharacter> makeCharacter(std::string type);
+		CharacterFactory(); 
+		ICharacter* makeCharacter(std::string type);
 };
-
 #endif	/* CHARACTERFACTORY_H */
-
