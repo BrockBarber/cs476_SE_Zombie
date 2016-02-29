00a5b755140e5a50ee82ed02a89a32ffbe657371
Brock Barber <barberbt22@uww.edu>
2016-02-29 17:01:02 -0600

    Update ZombieWar.h

--- zombiewar_cpp/ZombieWar.h
+++ zombiewar_cpp/ZombieWar.h
@@ -1,21 +1,25 @@
-/* 
- * File:   ZombieWar.h
- * Author: Thao, Brock
- *
- * Created on May 21, 2015, 1:54 PM
- */
+/*
+* File:   ZombieWar.h
+* Author: Thao, Brock
+*
+* Created on May 21, 2015, 1:54 PM
+*/
 
 #ifndef ZOMBIEWAR_H
 #define	ZOMBIEWAR_H
 
+#include "CharacterFactory.h"
 #include "ICharacter.h"
 #include "ISurvivor.h"
 #include "IZombie.h"
-	const NUM_ZOMBIE_TYPE = 1;
-	const NUM_SURVIVOR_TYPE = 1;
-	randomZombies();
-	randomSurvivors();
-	bool allDead(ICharacter * array);
-	void start();
+#include <memory>
+#include <vector>
+
+const int NUM_ZOMBIE_TYPE = 1;
+const int NUM_SURVIVOR_TYPE = 1;
+void randomZombies();
+void randomSurvivors();
+bool allDead(std::vector<std::unique_ptr<ICharacter>>);
+void start();
 
 #endif	/* ZOMBIEWAR_H */
